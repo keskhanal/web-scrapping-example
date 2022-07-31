7d413086072d9e0c6c08113669bbed3e7ef9bf1e
keskhanal <keskhanal2413@gmail.com>
2022-07-31 09:58:23 +0545

    added readme file

--- main.py
+++ main.py
@@ -1,48 +1,76 @@
-from urllib.request import urlopen as ureq
-from bs4 import BeautifulSoup as soup
+#import libraries
+import pandas as pd
+import numpy as np
+import requests
+import bs4 as BeautifulSoup
 
-my_url = 'https://www.newegg.com/p/pl?d=graphic+card'
+my_url = 'https://www.ambitionbox.com/list-of-companies?page=1'
 
 #Opening up connection, grabbing the page
-page = ureq(my_url)
-page_html = page.read()
-page.close()
+webpage = requests.get(my_url).text
+soup = BeautifulSoup.BeautifulSoup(webpage,'lxml')
 
-#html parsing
-page_soup = soup(page_html, "html.parser")
-
-#grabs each products
-containers = page_soup.findAll("div",{"class":"item-container"})
-
-file_name = "products.csv"
-f = open(file_name, "w")
-
-#headers of the file
-headers = "brand, product_name, price, shipping_cost\n"
-f.write(headers)
- 
-for container in containers:
-    brand = ""
-    try:
-       brand = container.div.div.a.img["title"].title()
-    except:
-       pass
-    # brand = container.div.div.a.img["title"].title()
-
-    title_container = container.findAll("a",{"class":"item-title"})
-    product_name = title_container[0].text
-
-    price_container = container.findAll("li",{"class":"price-current"})
-    price = price_container[0].text
-    
-    shipping_container = container.findAll("li", {"class":"price-ship"})
-    shipping_price = shipping_container[0].text
-    
-    # print(f"Brand name of product: {brand}")   
-    # print(f"Name of the product: {product_name}")
-    # print(f"Price of product: {price}")
-    # print(f"shipping price: {shipping_price}")
-
-    f.write(brand + "," + product_name.replace(",","-") + "," + price + "," + shipping_price + "\n")
-
-f.close()
+final_df=pd.DataFrame()
+for j in range(1,5):
+   webpage=requests.get('https://www.ambitionbox.com/list-of-companies?page={}'.format(j)).text
+   soup = BeautifulSoup.BeautifulSoup(webpage,'lxml')
+   
+   company=soup.find_all('div',class_='company-content-wrapper')
+   
+   name=[]
+   rating=[]
+   reviews=[]
+   ctype=[]
+   hq=[]
+   how_old=[]
+   no_of_employee=[]
+   
+   for i in company:
+      try:
+         name.append(i.find('h2').text.strip())
+      except:
+         name.append(np.nan)
+      
+      try:
+         rating.append(i.find('p',class_='rating').text.strip())
+      except:
+         rating.append(np.nan)
+      
+      try:
+        reviews.append(i.find('a' , class_='review-count').text.strip())
+      except:
+        reviews.append(np.nan)
+      
+      try:
+        ctype.append(i.find_all('p',class_='infoEntity')[0].text.strip())
+      except:
+          ctype.append(np.nan)
+      
+      try:
+        hq.append(i.find_all('p',class_='infoEntity')[1].text.strip())
+      except:
+        hq.append(np.nan)
+      
+      try:
+        how_old.append(i.find_all('p',class_='infoEntity')[2].text.strip())
+      except:
+        how_old.append(np.nan)
+      
+      try:
+         no_of_employee.append(i.find_all('p',class_='infoEntity')[3].text.strip())
+      except:
+        no_of_employee.append(np.nan)
+      
+      df=pd.DataFrame({
+          'name':name,
+          'rating':rating,
+          'reviews':reviews,
+          'company_type':ctype,
+          'Head_Quarters':hq,
+          'Company_Age':how_old,
+          'No_of_Employee':no_of_employee,
+        })
+      
+      final_df=pd.concat([final_df, df], ignore_index=True)
+      
+final_df.to_csv("company.csv", sep='\t', encoding='utf-8')
